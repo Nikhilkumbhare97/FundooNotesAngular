5a1fa9132a04580c944b6646bc63533525448af9
nikhilkumbhare <nikhilk190697@gmail.com>
2021-05-27 01:15:42 +0530

    Added: Validation Part of Login Page

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+const PASSWORD_REGEX = new RegExp('^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=[^$@!#%*?&]*[$#@!%*?&][^$@!#%*?&]*$).{8,}');
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
   constructor() { }
 
+  form = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.pattern(PASSWORD_REGEX)]),
+  })
+
+
   ngOnInit(): void {
+  }
+
+  submit(): void {
+    console.log("error");
   }
 
 }
